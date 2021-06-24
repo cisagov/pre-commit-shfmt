f88f263dd64cd12f641a3d611b9fdf792a173f33
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2021-06-23 22:49:18 -0400

    Use hook config and support parallelized running
    Move the default options from the script to the hook's configuration. Make some
    changes to provide a better experience when pre-commit runs the hook in
    parallel.

--- hooks/shfmt.sh
+++ hooks/shfmt.sh
@@ -10,28 +10,19 @@ then
   exit 1
 fi
 
-files_modified=0
-# Default options to list files changed and write changes to files
-base_options=("-l" "-w")
-
-# Combine the base options with what is passed in from pre-commit
-shfmt_options=("${base_options[@]}" "$@")
 shfmt_output=()
 
 while read -r line
 do
   shfmt_output+=("$line")
-done < <(shfmt "${shfmt_options[@]}")
+done < <(shfmt "$@")
 
-files_modified=${#shfmt_output[@]}
-
-if [[ $files_modified -ne 0 ]]
+if [[ ${#shfmt_output[@]} -ne 0 ]]
 then
   for file in "${shfmt_output[@]}"
   do
     echo "$file"
   done
 
-  echo "$files_modified file(s) modified"
   exit 1
 fi
